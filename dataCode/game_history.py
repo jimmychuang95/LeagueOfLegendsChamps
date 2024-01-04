4eb49201aa747b5f6b1241d802807449107f36f2
SHAWN! <t108820020@ntut.org.tw>
2024-01-04 12:07:11 +0800

    fix: fix win_rate error and redefine the definition of pick_rate

--- dataCode/game_history.py
+++ dataCode/game_history.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 
 #NOTE: change summoner_name_for_save_path to your summoner name
-summoner_name_for_save_path = "Owner"
+summoner_name_for_save_path = "Zeus"
 
 champion_file_path = "../data/champions/champions.json"
 with open(champion_file_path, 'r') as file:
@@ -45,15 +45,18 @@ def update_win_pick_num(position_champion_win_rate_df, summoner_team, summoner_c
         if summoner_winFlag == True: # if win   
             position_champion_win_rate_df.loc[summoner_champion.lower(), opponent_champion.lower()][0] += 1 # win+=1
             position_champion_win_rate_df.loc[summoner_champion.lower(), "total"][0] += 1 # win+=1
-        win_rate = position_champion_win_rate_df.loc[summoner_champion.lower(), opponent_champion.lower()][0] / position_champion_win_rate_df.loc[summoner_champion.lower(), opponent_champion.lower()][1] # win_rate = win/pick
-        pick_rate = position_champion_win_rate_df.loc[summoner_champion.lower(), opponent_champion.lower()][1] / num_of_games # pick_rate = pick/num_of_games
-        position_champion_win_rate_df.loc[summoner_champion.lower(), opponent_champion.lower()][2] = f"{win_rate:.2%}"
-        position_champion_win_rate_df.loc[summoner_champion.lower(), opponent_champion.lower()][3] = f"{pick_rate:.2%}"
+        win_rate = position_champion_win_rate_df.loc[summoner_champion.lower(), opponent_champion.lower()][0] / position_champion_win_rate_df.loc[summoner_champion.lower(), opponent_champion.lower()][1] * 100# win_rate = win/pick
+        position_champion_win_rate_df.loc[summoner_champion.lower(), opponent_champion.lower()][2] = round(win_rate, 2)
 
-        total_win_rate = position_champion_win_rate_df.loc[summoner_champion.lower(), "total"][0] / position_champion_win_rate_df.loc[summoner_champion.lower(), "total"][1] # win_rate = win/pick
-        total_pick_rate = position_champion_win_rate_df.loc[summoner_champion.lower(), "total"][1] / num_of_games # pick_rate = pick/num_of_games
-        position_champion_win_rate_df.loc[summoner_champion.lower(), "total"][2] = f"{total_win_rate:.2%}"
-        position_champion_win_rate_df.loc[summoner_champion.lower(), "total"][3] = f"{total_pick_rate:.2%}"
+        total_win_rate = position_champion_win_rate_df.loc[summoner_champion.lower(), "total"][0] / position_champion_win_rate_df.loc[summoner_champion.lower(), "total"][1] * 100 # win_rate = win/pick
+        position_champion_win_rate_df.loc[summoner_champion.lower(), "total"][2] = round(total_win_rate, 2)
+
+def calculate_total_pick_rate(position_champion_win_rate_df, num_of_games):
+    for i, row in position_champion_win_rate_df.iterrows():
+        total_pick_rate = row['total'][1] / num_of_games *100
+        row['total'][3] =  round(total_pick_rate, 2)
+        # position_champion_win_rate_df.at[i, 'total'] = total_list
+        # print(position_champion_win_rate_df.at[i, 'total'])
 
 def remove_spec_row_col(target_df, pattern):
     # Drop rows where all values match the pattern
@@ -61,6 +64,17 @@ def remove_spec_row_col(target_df, pattern):
     # Drop columns where all values match the pattern
     target_df = target_df.loc[:, ~target_df.isin([pattern]).all(axis=0)]
     return target_df
+
+#NOTE:pick_rate definition: 在對方使用該角色的場次中，我方使用此角色的場次佔比
+def calculate_pick_rate(position_champion_win_rate_df):
+    for column in position_champion_win_rate_df.columns:
+        # 計算每一列中所有陣列的第二個元素的總和
+        opponent_spec_champion_num = sum(grid[1] for grid in position_champion_win_rate_df[column]) #對方使用該角色的場次
+        if opponent_spec_champion_num != 0:# avoid divided by zero
+            for i, row in position_champion_win_rate_df.iterrows():
+                pick_rate = row[column][1] / opponent_spec_champion_num * 100 # pick_rate = pick/opponent_spec_champion_num
+                row[column][3] = round(pick_rate, 2)
+
 
 for summoner_team, summoner_position, summoner_champion, summoner_winFlag, opponent_champion in zip(summoner_teams, summoner_positions, summoner_champions, summoner_winFlags, opponent_champions):
     if summoner_position == 'TOP':
@@ -79,7 +93,21 @@ for summoner_team, summoner_position, summoner_champion, summoner_winFlag, oppon
         support_game_num += 1
         update_win_pick_num(support_champion_win_rate_df, summoner_team, summoner_champion, opponent_champion, summoner_winFlag, support_game_num)
 
+#caculate total_pick_rate
+calculate_total_pick_rate(top_champion_win_rate_df, top_game_num)
+calculate_total_pick_rate(jungle_champion_win_rate_df, jungle_game_num)
+calculate_total_pick_rate(mid_champion_win_rate_df, mid_game_num)
+calculate_total_pick_rate(adc_champion_win_rate_df, adc_game_num)
+calculate_total_pick_rate(support_champion_win_rate_df, support_game_num)
 
+calculate_pick_rate(top_champion_win_rate_df)
+calculate_pick_rate(jungle_champion_win_rate_df)
+calculate_pick_rate(mid_champion_win_rate_df)
+calculate_pick_rate(adc_champion_win_rate_df)
+calculate_pick_rate(support_champion_win_rate_df)
+
+
+#remove all empty row and col
 top_champion_win_rate_df = remove_spec_row_col(top_champion_win_rate_df, [0,0,0,0])
 jungle_champion_win_rate_df = remove_spec_row_col(jungle_champion_win_rate_df, [0,0,0,0])
 mid_champion_win_rate_df = remove_spec_row_col(mid_champion_win_rate_df, [0,0,0,0])
